e68b078b3f876b172cc01054d6036fc5d18b0b91
Zhuuoyue Zhao <zyzhao@cs.utah.edu>
2020-01-16 15:31:26 -0700

    update

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ CPPFLAGS =
 LDFLAGS =
 
 EXES=test_hh driver
-OBJS=test_pams.o sketch.o test_hh.o conf.o exact_query.o misra_gries.o pcm.o pla.o sampling.o heavyhitters.o test_pcm.o driver.o pmmg.o test_pla.o test_conf.o MurmurHash3.o pams.o 
+OBJS=test_pams.o sketch.o test_hh.o conf.o old_driver.o exact_query.o misra_gries.o pcm.o pla.o sampling.o heavyhitters.o test_pcm.o driver.o pmmg.o test_pla.o test_conf.o MurmurHash3.o perf_timer.o pams.o 
 
 .PHONY: all clean depend
 
@@ -22,7 +22,7 @@ test_pams: test_pams.o pams.o conf.o MurmurHash3.o
 
 test_hh: test_hh.o heavyhitters.o pcm.o pla.o conf.o MurmurHash3.o
 
-driver: driver.o pla.o pcm.o pams.o sampling.o heavyhitters.o sketch.o exact_query.o conf.o misra_gries.o pmmg.o MurmurHash3.o
+driver: driver.o pla.o pcm.o pams.o sampling.o heavyhitters.o sketch.o exact_query.o conf.o misra_gries.o pmmg.o MurmurHash3.o old_driver.cpp perf_timer.o
 
 # objs
 
@@ -36,6 +36,8 @@ test_hh.o: test_hh.cpp heavyhitters.h pcm.h pla.h util.h sketch.h \
  MurmurHash3.h
 
 conf.o: conf.cpp conf.h hashtable.h util.h config_list.h
+
+old_driver.o: old_driver.cpp sketch.h util.h
 
 exact_query.o: exact_query.cpp exact_query.h sketch.h util.h conf.h \
  hashtable.h
@@ -54,7 +56,8 @@ heavyhitters.o: heavyhitters.cpp heavyhitters.h pcm.h pla.h util.h \
 
 test_pcm.o: test_pcm.cpp pcm.h pla.h util.h sketch.h MurmurHash3.h
 
-driver.o: driver.cpp sketch.h util.h conf.h hashtable.h misra_gries.h
+driver.o: driver.cpp sketch.h util.h conf.h hashtable.h misra_gries.h \
+ perf_timer.h
 
 pmmg.o: pmmg.cpp pmmg.h util.h misra_gries.h hashtable.h sketch.h conf.h
 
@@ -63,6 +66,8 @@ test_pla.o: test_pla.cpp pla.h
 test_conf.o: test_conf.cpp conf.h hashtable.h
 
 MurmurHash3.o: MurmurHash3.cpp MurmurHash3.h
+
+perf_timer.o: perf_timer.cpp perf_timer.h
 
 pams.o: pams.cpp pams.h util.h sketch.h MurmurHash3.h conf.h hashtable.h
 
--- driver.cpp
+++ driver.cpp
@@ -15,308 +15,14 @@
 #include "conf.h"
 #include "misra_gries.h"
 #include <iomanip>
+#include "perf_timer.h"
 
-using namespace std;
+//using namespace std;
 
-/* old impl. */
-void print_help(const char *progname, const char *help_str = nullptr) {
-    cout << "usage: " << progname << " <QueryType> <SketchType> <infile>";
-    if (help_str)
-    {
-        cout << help_str; 
-    }
-    else
-    {
-        /* We have argc <= 2 at this point */
-        cout << " <params...>";
-        cout << endl;
-        cout << "Hint: enter query type to view the list of supported sketch types" << endl;
-        cout << "\navailable query types:" << endl;
-        cout << "\tpoint_interval" << endl;
-        cout << "\tpoint_att" << endl;
-        cout << "\theavy_hitter" << endl;
-    }
-}
+/* old impl. moved to old_driver.cpp */
+int old_main(int argc, char *argv[]);
 
-std::tuple<unsigned long long, unsigned long long, std::string>
-parse_point_interval_query(const std::string &line) {
-    std::istringstream in(line);
-
-    char c;
-    unsigned long long ts_s, ts_e;
-    std::string str;
-    in >> c >> ts_s >> ts_e >> str;
-    return std::make_tuple(ts_s, ts_e, str);
-}
-
-const char*
-test_point_interval(
-    IPersistentSketch *sketch,
-    const char *infile_name) {
-
-    IPersistentPointQueryable *ippq = dynamic_cast<IPersistentPointQueryable*>(sketch);
-    assert(ippq);
-    
-    std::ifstream infile(infile_name); 
-    if (!infile) {
-        return "\n[ERROR] Unable to open input file\n";
-    }
-
-    std::unordered_map<std::string, std::set<unsigned long long>> cnt;
-    
-    unsigned long long ts = 0;
-    std::string line;
-    while (std::getline(infile, line), !line.empty()) {
-        if (line[0] == '?') {
-            unsigned long long ts_s, ts_e;
-            std::string str;
-            std::tie(ts_s, ts_e, str) = parse_point_interval_query(line);
-            
-            double est_value = ippq->estimate_point_in_interval(str.c_str(), ts_s, ts_e);
-            unsigned long long true_value = 0;
-            auto &ts_set = cnt[str];
-            for (auto iter = ts_set.upper_bound(ts_s);
-                    iter != ts_set.end() && *iter <= ts_e;
-                    ++iter) ++true_value;
-
-            cout << str << "[" << ts_s << "," << ts_e << "]:\tEst: "
-                << est_value << "\tTruth: " <<  true_value << endl; 
-        } else {
-            ippq->update(++ts, line.c_str());
-            cnt[line].emplace(ts);
-        } // TODO turnstile: prefix the input with '-'
-    }
-
-    cout << "memory_usage() == " << ippq->memory_usage() << endl;
-
-    return nullptr;
-}
-
-std::tuple<unsigned long long, std::string>
-parse_point_att_query(const std::string &line) {
-    std::istringstream in(line);
-
-    char c;
-    unsigned long long ts_e;
-    std::string str;
-    in >> c >> ts_e >> str;
-    return std::make_tuple(ts_e, str);
-}
-
-const char*
-test_point_att(
-    IPersistentSketch *sketch,
-    const char *infile_name) {
-
-    IPersistentPointQueryable *ippq = dynamic_cast<IPersistentPointQueryable*>(sketch);
-    assert(ippq);
-    
-    
-    std::ifstream infile(infile_name);
-    if (!infile) {
-        return "\n[ERROR] Unable to open input file\n";
-    }
-
-    std::unordered_map<std::string, std::set<unsigned long long>> cnt;
-
-    unsigned long long ts = 0;
-    std::string line;
-    while (std::getline(infile, line), !line.empty()) {
-        if (line[0] == '?') {
-            unsigned long long ts_e;
-            std::string str;
-            std::tie(ts_e, str) = parse_point_att_query(line);
-            
-            double est_value = ippq->estimate_point_at_the_time(str.c_str(), ts_e);
-            unsigned long long true_value = 0;
-            auto &ts_set = cnt[str];
-            for (auto iter = ts_set.begin();
-                    iter != ts_set.end() && *iter <= ts_e;
-                    ++iter) ++true_value;
-
-            cout << str << "[0," << ts_e << "]:\tEst: "
-                << est_value << "\tTruth: " <<  true_value << endl; 
-        } else {
-            ippq->update(++ts, line.c_str());
-            cnt[line].emplace(ts);
-        } // TODO turnstile: prefix the input with '-'
-    }
-
-    cout << "memory_usage() == " << ippq->memory_usage() << endl;
-
-    return nullptr;
-}
-
-const char*
-test_heavy_hitter(
-    IPersistentSketch *sketch,
-    const char *infile_name)
-{
-    IPersistentHeavyHitterSketch *iphh =
-        dynamic_cast<IPersistentHeavyHitterSketch*>(sketch);
-    assert(iphh);
-
-    std::ifstream infile(infile_name);
-    if (!infile)
-    {
-        return "\n[ERROR] Unable to open input file\n";
-    }
-    
-    std::string line;
-    while (std::getline(infile, line), !line.empty()) {
-        if (line[0] == '?')
-        {
-            TIMESTAMP ts_e;
-            double fraction;
-            sscanf(line.c_str(), "? %llu %lf", &ts_e, &fraction);
-            auto estimated = iphh->estimate_heavy_hitters(ts_e, fraction);
-            cout << "HeavyHitter(" << fraction << "|" << ts_e << ") = {" << endl;
-            for (const auto &hh: estimated)
-            {
-                struct in_addr ip = { .s_addr = (in_addr_t) hh.m_value };
-                
-                cout << '\t' << inet_ntoa(ip) << ' ' << hh.m_fraction << endl;
-            }
-            cout << '}' << endl;
-        }
-        else
-        {
-            TIMESTAMP ts;
-            char ip_str[17];
-            struct in_addr ip;
-
-            sscanf(line.c_str(), "%llu %16s", &ts, ip_str);
-            if (!inet_aton(ip_str, &ip))
-            {
-                cout << "[WARN] Malformatted line: " << line << endl;
-                continue;
-            }
-            iphh->update(ts, (uint32_t) ip.s_addr);
-        }
-    }
-
-    return nullptr;
-}
-
-int old_main(int argc, char **argv) {
-
-    setup_sketch_lib();
-
-    int argi = 0;
-    const char *progname = argv[argi++];
-    if (argc < 2) {
-        print_help(progname);
-        return 1;
-    }
-
-    const char *help_str = nullptr;
-    const char *query_type = argv[argi++];
-    std::vector<SKETCH_TYPE> supported_sketch_types =
-        check_query_type(query_type, &help_str);
-    if (help_str)
-    {
-        print_help(progname, help_str);
-        return 1;
-    }
-
-    auto get_supported_sketch_types_helpstr= [&](ssize_t &off) -> const char *
-    {
-
-        if (off >= help_str_bufsize)
-        {
-            help_str_buffer[help_str_bufsize - 1] = '\0';
-            return help_str_buffer;
-        }
-
-        off += snprintf(help_str_buffer + off, help_str_bufsize - off,
-            "\nSupported sketch types for %s:\n", query_type);
-        if (off >= help_str_bufsize)
-        {
-            help_str_buffer[help_str_bufsize - 1] = '\0';
-            return help_str_buffer;
-        }
-
-        for (auto st: supported_sketch_types)
-        {
-            off += snprintf(help_str_buffer + off, help_str_bufsize - off,
-                "\t%s (%s)\n", sketch_type_to_sketch_name(st),
-                sketch_type_to_altname(st));
-            if (off >= help_str_bufsize)
-            {
-                help_str_buffer[help_str_bufsize - 1] = '\0';
-                break;
-            }
-        }
-        return help_str_buffer; 
-    };
-
-    if (argc < 3)
-    {
-        ssize_t off = snprintf(help_str_buffer, help_str_bufsize,
-            " <params...>\n");
-        help_str = get_supported_sketch_types_helpstr(off);
-        print_help(progname, help_str);
-        return 1;
-    }
-    
-    const char *sketch_name = argv[argi++];
-    SKETCH_TYPE st = sketch_name_to_sketch_type(sketch_name);
-    if (st == ST_INVALID) {
-        ssize_t off = snprintf(help_str_buffer, help_str_bufsize,
-            " <params...>\n[ERROR] Unknown sketch type: %s\n", sketch_name);
-        help_str = get_supported_sketch_types_helpstr(off);
-        print_help(progname, help_str);
-        return 1;
-    }
-
-    if (supported_sketch_types.end() == 
-        std::find(supported_sketch_types.begin(), supported_sketch_types.end(), st))
-    {
-        ssize_t off = snprintf(help_str_buffer, help_str_bufsize,
-            " <params...>\n[ERROR] Unsupported sketch type: %s\n", sketch_name);
-        help_str = get_supported_sketch_types_helpstr(off);
-        print_help(progname, help_str);
-        return 1;
-    }
-
-    if (argi >= argc)
-    {
-        print_help(progname, " <params...>\n[ERROR] missing input file\n");
-        return 1;
-    }
-    const char *infile_name = argv[argi++];
-
-    IPersistentSketch *sketch =
-        create_persistent_sketch(st, argi, argc, argv, &help_str);
-    if (!sketch) {
-        print_help(progname, help_str);
-        return 1;
-    }
-    ResourceGuard guard(sketch);
-
-    help_str = nullptr; 
-    if (!strcmp(query_type, "point_interval")) {
-        help_str =
-            test_point_interval(sketch, infile_name);
-    } else if (!strcmp(query_type, "point_att")) {
-        help_str =
-            test_point_att(sketch, infile_name);
-    } else if (!strcmp(query_type, "heavy_hitter")) {
-        help_str =
-            test_heavy_hitter(sketch, infile_name);
-    } else {
-        print_help(progname);
-        return 1;
-    }
-    
-    if (help_str)
-    {
-        print_help(progname, help_str);
-        return 2;
-    }
-    return 0;
-}
-
+/* new impl. */
 std::string format_outfile_name(
     const std::string &outfile_name,
     const char *time_text,
@@ -350,7 +56,16 @@ std::string format_outfile_name(
     return std::move(ret);
 }
 
-/* new impl. */
+#define PERF_TIMER_TIMEIT(timer, action) \
+    do { \
+        if (!do_measure_time) { action } else { \
+            (timer)->measure_start(); \
+            { action } \
+            (timer)->measure_end();   \
+        } \
+    } while (0)
+
+
 int run_new_heavy_hitter()
 {
     std::time_t tt = std::time(nullptr);
@@ -378,12 +93,6 @@ int run_new_heavy_hitter()
         return 1;
     }
 
-    std::cerr << "Running query heavy_hitter at " << text_tt << std::endl;
-    if (0 == strftime(text_tt, 256, "%Y-%m-%d-%H-%M-%S", &local_time))
-    {
-        strcpy(text_tt, "<time_buffer_too_small>");
-    }
-
     std::vector<SKETCH_TYPE> supported_sketch_types =
         check_query_type("heavy_hitter", nullptr);
     
@@ -407,6 +116,22 @@ int run_new_heavy_hitter()
             }
         }
     }
+
+    std::vector<PerfTimer> update_timers;
+    std::vector<PerfTimer> query_timers;
+    bool do_measure_time = g_config->get_boolean("perf.measure_time").value();
+    if (do_measure_time)
+    {
+        update_timers.resize(sketches.size());
+        query_timers.resize(sketches.size());
+    }
+
+    std::cerr << "Running query heavy_hitter at " << text_tt << std::endl;
+    if (0 == strftime(text_tt, 256, "%Y-%m-%d-%H-%M-%S", &local_time))
+    {
+        strcpy(text_tt, "<time_buffer_too_small>");
+    }
+
     
     std::string infile_name = g_config->get("infile").value();
     std::optional<std::string> outfile_name_opt = g_config->get("outfile");
@@ -446,9 +171,12 @@ int run_new_heavy_hitter()
             std::unordered_set<uint32_t> exact_answer_set;
             if (exact_pos != -1)
             {
-                exact_answer = 
-                    sketches[exact_pos].get()->estimate_heavy_hitters(
-                        ts_e, fraction);
+                PERF_TIMER_TIMEIT(&query_timers[exact_pos],
+                {
+                    exact_answer = 
+                        sketches[exact_pos].get()->estimate_heavy_hitters(
+                            ts_e, fraction);
+                });
                 std::transform(exact_answer.begin(), exact_answer.end(),
                         std::inserter(exact_answer_set, exact_answer_set.end()),
                         [](const auto &hh) -> uint32_t {
@@ -464,9 +192,11 @@ int run_new_heavy_hitter()
 
                 if (i != exact_pos)
                 {
-                    answer =
-                        sketches[i].get()->estimate_heavy_hitters(
+                    PERF_TIMER_TIMEIT(&query_timers[i],
+                    {
+                        answer = sketches[i].get()->estimate_heavy_hitters(
                             ts_e, fraction);
+                    });
                     
                     if (exact_pos != -1)
                     {
@@ -537,7 +267,7 @@ int run_new_heavy_hitter()
                 sscanf(line.c_str(), "%llu %16s", &ts, ip_str);
                 if (!inet_aton(ip_str, &ip))
                 {
-                    cerr << "[WARN] Malformatted line: " << line << endl;
+                    std::cerr << "[WARN] Malformatted line: " << line << std::endl;
                     continue;
                 }
                 value = (uint32_t) ip.s_addr;
@@ -546,9 +276,13 @@ int run_new_heavy_hitter()
             {
                 sscanf(line.c_str(), "%llu %u", &ts, &value);
             }
-            for (auto &rg_ipph: sketches)
+
+            for (auto i = 0u; i < sketches.size(); ++i)
             {
-                rg_ipph.get()->update(ts, value);
+                PERF_TIMER_TIMEIT(&update_timers[i],
+                {
+                    sketches[i].get()->update(ts, value);
+                });
             }
 
             ++n_data;
@@ -560,7 +294,8 @@ int run_new_heavy_hitter()
         }
     }
     
-    std::cout << "memory_usage() = " << std::endl;
+    std::cout << std::endl;
+    std::cout << "=============  Memory Usage  =============" << std::endl;
     for (auto &rg_ipph: sketches)
     {
         size_t mm_b = rg_ipph.get()->memory_usage();
@@ -577,6 +312,36 @@ int run_new_heavy_hitter()
              << std::setfill(saved_fill)
              << " MB"
              << std::endl;
+    }
+
+    if (do_measure_time)
+    {
+        std::cout << "=============  Time stats    =============" << std::endl;
+
+        std::cout << "Update timers:" << std::endl;
+        for (auto i = 0u; i < sketches.size(); ++i)
+        {
+            std::cout << '\t'
+                << sketches[i].get()->get_short_description()
+                << ": tot = "
+                << update_timers[i].get_elapsed_ms() << " ms = "
+                << update_timers[i].get_elapsed_s() << " s (avg "
+                << update_timers[i].get_avg_elapsed_us() << " us = "
+                << update_timers[i].get_avg_elapsed_ms() << " ms)"
+                << std::endl;
+        }
+        std::cout << "Query timers:" << std::endl;
+        for (auto i = 0u; i < sketches.size(); ++i)
+        {
+            std::cout << '\t'
+                << sketches[i].get()->get_short_description()
+                << ": tot = "
+                << query_timers[i].get_elapsed_ms() << " ms = "
+                << query_timers[i].get_elapsed_s() << " s (avg "
+                << query_timers[i].get_avg_elapsed_us() << " us = "
+                << query_timers[i].get_avg_elapsed_ms() << " ms)"
+                << std::endl;
+        }
     }
 
     return 0;
@@ -769,7 +534,7 @@ int run_new_heavy_hitter_bitp()
                 sscanf(line.c_str(), "%llu %16s", &ts, ip_str);
                 if (!inet_aton(ip_str, &ip))
                 {
-                    cerr << "[WARN] Malformatted line: " << line << endl;
+                    std::cerr << "[WARN] Malformatted line: " << line << std::endl;
                     continue;
                 }
                 value = (uint32_t) ip.s_addr;
