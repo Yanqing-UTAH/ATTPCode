2e4b3a1fca90315a5351cc48fc5d6d311449b703
Yanqing Peng <ypeng@cs.utah.edu>
2020-07-05 23:17:53 +0800

    add plots

---
+++ plot/download.sh
@@ -0,0 +1,30 @@
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven6/zyzhao/ATTPCode/output/test-client-id-all-stats-05.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven4/zyzhao/ATTPCode/output/test-client-id-all-stats-06.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/ravenserv1/zyzhao/ATTPCode/output/test-client-id-all-stats.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven4/zyzhao/ATTPCode/output/test-client-id-all-stats-03.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven3/zyzhao/ATTPCode/output/test-client-id-all-stats-02.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven1/zyzhao/ATTPCode/output/test-client-id-all-stats-00.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven5/zyzhao/ATTPCode/output/test-client-id-all-stats-04.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven2/zyzhao/ATTPCode/output/test-client-id-all-stats-01.txt .
+
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven8/zyzhao/ATTPCode/output/test-object-id-all-stats-00.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven1/zyzhao/ATTPCode/output/test-object-id-all-stats-05.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven8/zyzhao/ATTPCode/output/test-object-id-all-stats-01.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven5/zyzhao/ATTPCode/output/test-object-id-all-stats-02.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven3/zyzhao/ATTPCode/output/test-object-id-all-stats-03.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven2/zyzhao/ATTPCode/output/test-object-id-all-stats-04.txt .
+
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/ravenserv1/zyzhao/ATTPCode/output/test-client-id-bitp-all-stats.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/ravenserv1/zyzhao/ATTPCode/output/test-client-id-bitp-all-stats-02.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/ravenserv1/zyzhao/ATTPCode/output/test-client-id-bitp-all-stats-01.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/ravenserv1/zyzhao/ATTPCode/output/test-client-id-bitp-all-stats-03.txt .
+
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven10/zyzhao/ATTPCode/output/test-object-id-all-bitp-stats-01.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven9/zyzhao/ATTPCode/output/test-object-id-all-bitp-stats-02.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven11/zyzhao/ATTPCode/output/test-object-id-all-bitp-stats-03.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven11/zyzhao/ATTPCode/output/test-object-id-all-bitp-stats-04.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven12/zyzhao/ATTPCode/output/test-object-id-all-bitp-stats-05.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven13/zyzhao/ATTPCode/output/test-object-id-all-bitp-stats-06.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven14/zyzhao/ATTPCode/output/test-object-id-all-bitp-stats-07.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven15/zyzhao/ATTPCode/output/test-object-id-all-bitp-stats-08.txt .
+scp ypeng@ravenserv1.cs.utah.edu:/uusoc/scratch/raven16/zyzhao/ATTPCode/output/test-object-id-all-bitp-stats-09.txt .
